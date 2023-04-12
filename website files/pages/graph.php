dd14a899254ccfc3fe87a17c7e4c3ce3b5116d86
justingales <jgales@oakland.edu>
2023-04-11 20:12:07 -0400

    updated website

---
+++ website files/pages/graph.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <title>Document</title>
+</head>
+<?php
+$host = "34.132.166.21";
+$username = "root";
+$password = "password";
+$dbname = "csi4160-grot-db";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("connection failed: " . $conn->connect_error);
+}
+// Query the database for servo logs within the past 24 hours
+$query = "SELECT timestamp, description FROM servo_log WHERE timestamp >= DATE_SUB(NOW(), INTERVAL 1 DAY)";
+$result = mysqli_query($conn, $query);
+
+// Initialize arrays for chart data
+$labels = array();
+$pet_movement_data = array();
+$food_dispensed_data = array();
+
+// Loop through the query results and populate the arrays
+while ($row = mysqli_fetch_assoc($result)) {
+  // Format the timestamp as a string
+  $timestamp = date('Y-m-d H:i:s', strtotime($row['timestamp']));
+  
+  // Check if the timestamp is zero and the description is "Servo Opened" or "Servo Closed"
+  if ($row['description'] == "Servo Opened" || $row['description'] == "Servo Closed") {
+    // Skip adding the timestamp to the labels array
+    continue;
+  }
+  
+  // Add the timestamp to the labels array
+  $labels[] = $timestamp;
+  
+  // Check the description and add the data to the appropriate array
+  if ($row['description'] == "Pet Movement") {
+    $pet_movement_data[] = 1;
+    $food_dispensed_data[] = null;
+  } else if ($row['description'] == "Food dispensed") {
+    $pet_movement_data[] = null;
+    $food_dispensed_data[] = 1;
+  } else {
+    $pet_movement_data[] = null;
+    $food_dispensed_data[] = null;
+  }
+}
+
+// Close the database connection
+mysqli_close($conn);
+?>
+
+<!-- Create a bar chart using Chart.js -->
+<div style="width: 1500px;"
+     >
+  <canvas id="myChart"></canvas>
+</div>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: <?php echo json_encode($labels); ?>,
+    datasets: [{
+      label: 'Pet Movement',
+      backgroundColor: 'rgba(0, 0, 255, 0.5)', // blue color for "Pet Movement" bars
+      data: <?php echo json_encode($pet_movement_data); ?>
+    }, {
+      label: 'Food Dispensed',
+      backgroundColor: 'rgba(255, 0, 0, 0.5)', // red color for "Food dispensed" bars
+      data: <?php echo json_encode($food_dispensed_data); ?>
+    }]
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        type: 'time',
+        time: {
+          unit: 'hour',
+          displayFormats: {
+            hour: 'HH:mm'
+          },
+          tooltipFormat: 'YYYY-MM-DD HH:mm:ss'
+        },
+        ticks: {
+          maxRotation: 90,
+          minRotation: 90
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+</script>
--- website files/petfeeder.php
+++ website files/petfeeder.php
@@ -6,7 +6,7 @@
 
     <meta charset="utf-8" />
 
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="css/style.css" />
 
     <title>Team Catbug Pet Feeder</title>
 
@@ -21,11 +21,11 @@
         </ul>
 
     </div>
-<img src="catbug.png" alt="Catbug">
+<img src="images/catbug.png" alt="Catbug">
     
     <div class="logs">
 <h1>Pet Feeder Logs</h1> 
-
+<button><a href="pages/graph.php">Go to Graph</a></button>
 <?php
 
 $host = "34.132.166.21";
@@ -38,7 +38,7 @@ if ($conn->connect_error) {
   die("connection failed: " . $conn->connect_error);
 }
 
-    
+
 $select_query = "SELECT * FROM servo_log";
 $result = $conn->query($select_query);
 if ($result->num_rows > 0) {
@@ -59,6 +59,59 @@ while($row = $result->fetch_assoc())
 }
     echo "</table>";
     }
+
+
 ?>
         </div>
+<?php
+// SQL query to retrieve data from the database
+$sql = "SELECT description, timestamp FROM servo_log ORDER BY timestamp DESC LIMIT 10";
+$result = mysqli_query($conn, $sql);
+
+// Create two arrays to hold the labels and data for the chart
+$labels = array();
+$data = array();
+
+// Loop through the query results and populate the arrays
+while ($row = mysqli_fetch_assoc($result)) {
+  $labels[] = $row['timestamp'];
+  if ($row['description'] == "Pet Movement") {
+    $data[] = 10; // set fixed height for "Pet Movement" bars
+  } else if ($row['description'] == "Food dispensed") {
+    $data[] = 15; // set fixed height for "Food dispensed" bars
+  }
+}
+
+// Create a bar chart using Chart.js
+?>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script>
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: <?php echo json_encode($labels); ?>,
+    datasets: [{
+      label: 'Servo Log',
+      backgroundColor: 'rgba(0, 0, 255, 0.5)', // blue color for "Pet Movement" bars
+      data: <?php echo json_encode($data); ?>
+    }, {
+      label: 'Servo Log',
+      backgroundColor: 'rgba(255, 0, 0, 0.5)', // red color for "Food dispensed" bars
+      data: <?php echo json_encode($data); ?>
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  }
+});
+</script>
 </html>
